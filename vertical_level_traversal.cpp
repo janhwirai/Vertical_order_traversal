e181e1c3dbb6fc7e85d1e1f0d875dedb2e813e1f
JANHWI RAI <104694618+janhwirai@users.noreply.github.com>
2023-07-21 15:44:51 +0530

    Add files via upload

---
+++ vertical_level_traversal.cpp
@@ -0,0 +1,165 @@
+//Vertical Order Traversal
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+          
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+void printInorder(Node* root)
+{
+    if(!root)
+        return;
+        
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+class Solution{
+    public:
+    //Function to find the vertical order traversal of Binary Tree.
+    vector<int> verticalOrder(Node *root)
+    {
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        //we will map hd with level and list of nodes
+        map<int, map<int , vector<int> >> nodes;
+        //Since we are required hd along with the level
+        //in level order traversal
+        //We will make a queue to maintain the pair
+        queue<pair<Node*,pair<int,int> > > q;
+        //Now store values in the queue
+        //Initially both the hd and level are zero
+        q.push(make_pair(root,make_pair(0,0)));
+        while(!q.empty()){
+            //Take out the value present in the front and store it in temp
+            pair<Node* ,pair<int,int>>temp=q.front();
+            q.pop();
+            
+            Node* frontNode=temp.first;
+            int hd=temp.second.first;
+            int level=temp.second.second;
+            
+            nodes[hd][level].push_back(frontNode->data);
+            
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left,make_pair(hd-1,level+1)));
+            }
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right,make_pair(hd+1,level+1)));
+            }
+        }
+        for (auto i:nodes)
+        {
+            for(auto j:i.second)
+            {
+                for(auto k:j.second)
+                {
+                    ans.push_back(k);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Solution obj;
+    	Node* root = buildTree(s);
+    	
+    	vector <int> res = obj.verticalOrder(root);
+    	for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+	return 0;
+}
